d8549eef8830c05ef61251f99ebf4dc56fde5071
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-07-03 15:58:07 -0400

    Jul 03, 2026 - 03:58 PM

--- genesis.go
+++ genesis.go
@@ -9,6 +9,7 @@ import (
 // does not yet include signatures
 
 func main() {
+  // genesis validator used to sign the transaction and block
   validator := ledger.NewValidator()
   validator.Addr = "test_addr"
   validator.CertificateTx = "genesis"
@@ -17,6 +18,7 @@ func main() {
   validatorJSON, _ := validator.ToJSON()
   fmt.Printf("Genesis Validator:\n %s\n\n", validatorJSON)
 
+  // initital protocol parameters
   params := ledger.NewParams()
   params.Network = 0
   params.MaxBlockHeaderSize = 256
@@ -29,6 +31,7 @@ func main() {
   paramsJSON, _ := params.ToJSON()
   fmt.Printf("Genesis Params:\n %+s\n\n", paramsJSON)
 
+  //
   epoch := ledger.NewEpoch()
   epoch.Header.ID = 0
   epoch.Body.StartTime = 0
@@ -36,28 +39,33 @@ func main() {
   epoch.Body.PreviousHash = "0000"
   epoch.Body.Validators = append(epoch.Body.Validators, validator.Addr)
   epoch.Body.ProtocolVersion = 0
-  epoch.Hash()
+  epoch.Hash() // generate epoch hash
   fmt.Printf("Genesis Epoch:\n %+v\n\n", epoch)
 
+  // convert validator to cbor and format into TxData
   validatorData := ledger.TxData{}
   validatorData.Tag = "genesis_validator"
   validatorCBOR, _ := validator.ToCBOR()
   validatorData.Data = validatorCBOR
   validatorData.Type = 0
 
+  // convert parameters to cbor and format into TxData
   paramsData := ledger.TxData{}
   paramsData.Tag = "genesis_params"
   paramsCBOR, _ := params.ToCBOR()
   paramsData.Data = paramsCBOR
   paramsData.Type = 0
 
+  // add data into transaction body
   transaction := ledger.NewTransaction()
   transaction.Body.Data = append(transaction.Body.Data, validatorData)
   transaction.Body.Data = append(transaction.Body.Data, paramsData)
+  transaction.Hash() // generate transaction hash
 
   txJSON, _ := transaction.ToJSON()
   fmt.Printf("Genesis Transaction:\n %s\n\n", txJSON)
 
+  // add transaction into block body
   block := ledger.NewBlock()
   block.Header.ID = 0
   block.Header.Validator = validator.Addr
@@ -68,7 +76,7 @@ func main() {
   block.Body.Transactions = append(block.Body.Transactions, transaction)
   block.Body.Fees = 0
   block.Body.Timestamp = 0
-  block.Hash()
+  block.Hash() // generate block hash
 
   blockJSON, _ := block.ToJSON()
   fmt.Printf("Genesis Block:\n %s\n\n", blockJSON)
