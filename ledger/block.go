7b69b9b5b020b23c4ace451b45b4622ae3f6f551
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-07-02 21:30:23 -0400

    /usr/bin/github -u

--- ledger/block.go
+++ ledger/block.go
@@ -1,5 +1,11 @@
 package ledger
 
+import(
+  "fmt"
+  "crypto/sha256"
+  "github.com/fxamacker/cbor/v2"
+)
+
 type Block struct {
   Header BlockHeader `cbor: "header"`
   Body   BlockBody   `cbor: "body"`
@@ -20,4 +26,23 @@ type BlockBody struct {
   Transactions []Transaction `cbor: "transactions"`
   Fees         uint          `cbor: "fees"`
   Timestamp    uint          `cbor: "timestamp"`
+}
+
+func (block *Block) ToCBOR() ([]byte, error) {
+  cborBytes, err := cbor.Marshal(block)
+  if err != nil { return []byte, err }
+  return cborBytes, nil
+}
+
+func (block *Block) BodyToCBOR() ([]byte, error) {
+  cborBytes, err := cbor.Marshal(block.Body)
+  if err != nil { return []byte, err }
+  return cborBytes, nil
+}
+
+func (block *Block) Hash() error {
+  cborBytes, err := block.BodyToCBOR()
+  if err != nil { return err }
+  block.Header.Hash = fmt.Sprintf("%x", sha256.Sum256(cborBytes))
+  return nil
 }
