93969c9d9b067fbe7717959021503ff0f133e5ec
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-07-31 17:22:29 -0400

    Jul 31, 2026 - 05:22 PM

--- genesis.go
+++ genesis.go
@@ -2,6 +2,6 @@ package psl
 
 type Genesis struct {
   Seed        []byte      `cbor:"0,keyasint"`
-  Certificate Certificate `cbor:"1,keyasint"`
-  Params      Params      `cbor:"2,keyasint"`
+  Params      Params      `cbor:"1,keyasint"`
+  Certificate Certificate `cbor:"2,keyasint"`
 }
